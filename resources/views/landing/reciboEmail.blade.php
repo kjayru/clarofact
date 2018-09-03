baa79a52025f6c0fc16b6eccf9da7b04c856be02
kjayru <wiltinoco@gmail.com>
2018-09-03 17:44:16 -0500

    plantillas

--- resources/views/landing/reciboEmail.blade.php
+++ resources/views/landing/reciboEmail.blade.php
@@ -3,20 +3,7 @@
 @section('content')
 
 <div class="espacioHeader"></div>
-
-<section class="sliderSecundario bgCover">
-    <div class="limit">
-            <article class="sliderSecundario__texto">
-                    <strong>¿Qué es el recibo por email?</strong>
-            </article>
-            <aside class="sliderSecundario__texto--mobile">
-                    <strong>¿Qué es el recibo por email?</strong>
-            </aside>
-    </div>
-</section>
-
-
-<section class="pagina">
+<div>
 
     <ul class="pagina__breadcrumb limit">
         <li><a href="/">Inicio</a></li>/
@@ -26,7 +13,20 @@
         <li><a href="/">Inicio</a></li>/
             <li>Qué es el recibo por email</li>
     </ul>
+<div>
+<section class="sliderSecundario bgCover">
+    <div class="limit">
+            <article class="sliderSecundario__texto">
+                    <strong>¿Qué es el recibo por email?</strong>
+            </article>
+            <aside class="sliderSecundario__texto--mobile">
+                    <strong></strong>
+            </aside>
+    </div>
 </section>
+
+
+
 
 {!! $page->body !!}
 
